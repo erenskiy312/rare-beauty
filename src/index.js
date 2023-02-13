b5a066868f8adf8edfab3b9d76b06e985e812dce
Nura <supergirl@MacBook-Air-Nurajym.local>
2023-02-13 19:12:17 +0600

    artukul2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import Artikul1 from "./components/Artikul1/Artikul1";
+import "./Artikul.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Artikul1 />
   </React.StrictMode>
 );
